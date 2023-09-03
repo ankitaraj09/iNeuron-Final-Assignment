c1a189aab969fcbdbee0495ca61ee51b4036e6b3
ankitaraj04lawinzo <ankita.rajak@lawinzo.com>
2023-09-03 17:08:16 +0530

    First_Commit

---
+++ 02-Constructor/src/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+import Test.Child;
+
+public class Main {
+    public static void main(String[] args) {
+        Child child = new Child(); // Creating an object of the Child class
+    }
+}
