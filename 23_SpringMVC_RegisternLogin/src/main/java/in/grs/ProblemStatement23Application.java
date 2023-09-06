9821c006b8d6e3632c336b03f8b97e9106b246b9
ankitaraj09 <ankitaraj0904@gmail.com>
2023-09-07 00:06:08 +0530

    First_Commit

---
+++ 23_SpringMVC_RegisternLogin/src/main/java/in/grs/ProblemStatement23Application.java
@@ -0,0 +1,13 @@
+package in.grs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemStatement23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemStatement23Application.class, args);
+	}
+
+}
---
+++ 23_SpringMVC_RegisternLogin/src/main/java/in/grs/model/Student.java
@@ -0,0 +1,16 @@
+package in.grs.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+  private Integer id;
+  private String username;
+  private String email;
+  private String password;
+  
+}
