ab1d0248f5c64297ebbe2bbb6c291cf79d624c70
ankitaraj09 <ankitaraj0904@gmail.com>
2023-09-07 08:58:07 +0530

    First_Commit

---
+++ 14-GetServlet/src/main/java/in/ineuron/Test.java
@@ -0,0 +1,29 @@
+package in.ineuron;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Welcome
+ */
+@WebServlet("/welcome")
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String parameter = request.getParameter("name");
+		PrintWriter out = response.getWriter();
+		out.println("<h1> Welcome "+parameter+"!</h1>");
+	}
+
+}
